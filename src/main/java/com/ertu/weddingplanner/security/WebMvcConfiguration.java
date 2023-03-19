29bd41a85d55735602e1b05cee784f7e6bc3e077
ertu96 <48797125+ertu96@users.noreply.github.com>
2023-03-19 15:48:48 +0100

    modified WebMvcConfiguration

--- src/main/java/com/ertu/weddingplanner/security/WebMvcConfiguration.java
+++ src/main/java/com/ertu/weddingplanner/security/WebMvcConfiguration.java
@@ -2,9 +2,11 @@ package com.ertu.weddingplanner.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
